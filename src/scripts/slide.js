97e4f814f093b1b9473ae148aad4600e0cc2340f
HoangEZ <nguyenquochoang234@gmail.com>
2019-02-01 22:39:30 +0700

    HTML&CSS Bai 2

---
+++ src/scripts/slide.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.slide-content').slick({
+        prevArrow:'.chev-left',
+        nextArrow:'.chev-right',
+        dots:'true',
+        dotClass:'control-btn-list'
+    });
+})
