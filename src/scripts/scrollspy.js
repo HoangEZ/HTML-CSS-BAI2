fe42294c563f7fcdc836c7133440615301f3b24d
HoangEZ <nguyenquochoang234@gmail.com>
2019-02-01 14:38:16 +0700

    HTML & CSS Bai 2

--- src/scripts/scrollspy.js
+++ src/scripts/scrollspy.js
@@ -1,3 +1,28 @@
-$(window).load(function(){
-    
+$(window).on('load',function(){
+    var link = $('.menu-link');
+    var arr_top = [];
+    $.each(link,function(index,value){
+        var element = {};
+        element.id = value.dataset.id;
+        element.top = $(element.id).position().top;
+        arr_top.push(element);
+    })
+    var add_active = function(){
+        var top  = $(window).scrollTop();
+        for(var i=0;i<arr_top.length;i++){
+            if(top<arr_top[i].top){
+                break;
+            }
+        }
+        link.removeClass('active');
+        $(link[--i]).addClass('active');
+    }
+    add_active();
+    $(window).scroll(function(){
+        add_active();
+    })
+    link.click(function(e){
+        e.preventDefault();
+        $('html,body').stop().animate({scrollTop:$($(this).data('id')).position().top+10},500);
+    })
 })
