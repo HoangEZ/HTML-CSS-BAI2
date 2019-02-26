f4c5bcd8cfb61c4c298364b2b26421e55462c786
HoangEZ <nguyenquochoang234@gmail.com>
2019-02-26 14:53:24 +0700

    HTML&CSS Bai 2

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         removalDelay:300,
         mainClass:'mfp-fade'
       });
+      $('.btn-play').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+      });
 })
